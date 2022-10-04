0f96ce3a9746b4b8a5b5e677e8e746d3b6760bf5
marcelo lira <marcelolira142@gmail.com>
2022-10-04 10:18:35 -0600

    se agrego pitagoras.cpp

---
+++ constantes.cpp
@@ -0,0 +1,12 @@
+/*
+nombre del archivo: sumanumeros.cpp
+aoutor. marcelo
+lugear: itv
+intrucciones: esto es una plantilla */
+
+#include<iosstream<>
+
+using namespace std;
+
+int main ()
+return 0;
