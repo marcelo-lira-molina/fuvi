446fc24c8ea4821a050292a4f9393664f46ac172
Electronica 2022 <electronica2022@fuvi.net>
2022-10-06 09:32:57 -0600

    se agrego nuevo archivo

---
+++ manipulacion.cpp
@@ -0,0 +1,15 @@
+/*
+nombre dell archivo: manipuladores.cpp
+aoutor: marcelo lira
+lugar: itv
+*/
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()[
+
+cout << left; 
+cout << setw(20) <<"artista" << setw(20)<< "album"<<endl
+         <<"michael jackson"<<"thriller"; ]
